3a1961ae3947a680f3093219f4caddfd9013f7dd
MTLight <matthieu@mtlight.ca>
2024-03-19 14:46:44 -0400

    mis a jour le printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,44 +6,32 @@
 /*   By: mamauss <mamauss@student.42quebec.>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 08:12:24 by mamauss           #+#    #+#             */
-/*   Updated: 2024/03/14 12:33:41 by mamauss          ###   ########.fr       */
+/*   Updated: 2024/03/17 14:22:38 by mamauss          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 #include <unistd.h>
 #include <stdio.h>
 #include <stdarg.h>
-int	ft_putchar(char c)
-{
-	write(1, &c, 1);
-	return (1);
-}
 
-int	ft_putchar_arg(va_list args)
-{
-	char c;
-
-	c = (char)va_arg(args, int);
-	write (1, &c, 1);
-	return (1);
-}
 void	format_specifier(va_list args, char format)
 {
-	va_list	args_copy;
-	va_copy(args_copy, args);
+	//va_list	args_copy;
+	//va_copy(args_copy, args);
 
 	if (format == 'c')
-		ft_putchar_arg(args_copy);
-	/*else if (format == 's')
-	
-	else if (format == 'p' || format == 'x' || format == 'X')
-
+		ft_putchar_arg(args);
+	else if (format == 's')
+		ft_putstr_arg(args);
 	else if (format == 'd' || format == 'i')
-
+		ft_itoa(args);
+	/*else if (format == 'p' || format == 'x' || format == 'X')
+	
 	else if (format == 'u')
 
 	else if (format == '%')*/
 	
-	va_end(args_copy);
+	va_end(args);
 }
 int	ft_printf(char *str, ...)
 {
@@ -78,12 +66,21 @@ int	ft_printf(char *str, ...)
 int	main()
 {
 	char			c;  // char
-	char			*s; // pointeur
-	int				*p; // adresse de pointeur similaire a un hexadecimal
+	char			c2;
+	char			s[6] = "hello"; // pointeur
+	char			s2[10] = "goodbye";
+	int			i;
+	int			*p; // adresse de pointeur
 	unsigned int	x; // hexadecimal
 	unsigned int	X; // hexadecimal en majuscule
 	
 	c = 'A';
-	printf("le char a imprimer est %c\n", c);
-	ft_printf("ma fonction imprime %c le char\n", c);	
+	c2 = 'B';
+	i = 42;
+	printf("les char a imprimer sont %c et %c et %s\n", c, c2, s);
+	ft_printf("ma fonction imprime %c et %c et %s les chars\n", c, c2, s);	
+	printf("le string a imprimer est %s et %s\n", s, s2);
+	ft_printf("ma fonction imprime %s et %s les strings\n ", s, s2);
+	printf("Le int a imprimer est %d\n", i);
+	ft_printf("ma fonction imprime %d le int\n", i);
 }	
